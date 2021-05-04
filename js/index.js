360ca8465d55fad3ba3ff578e69e46e26d49ac52
JUANCHI1403 <carloschile20192019@gmail.com>
2021-05-04 13:57:17 -0400

    para acceder a la Api de Marvel se debe generar el password este caso se realiza con md5 donde se colocan las credenciales 1+privatekey+publickey

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const marvel = {
 
+    //Hash = c595a660873c0e2b441e38b562fe56ad
+    // establish 
+    // public 
     render: () => {
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a9ea64dfbeb843a83320b3acd179e2a3&hash=C595A660873C0E2B441E38B562FE56AD';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a9ea64dfbeb843a83320b3acd179e2a3&hash=c595a660873c0e2b441e38b562fe56ad';
         const container = document.querySelector('#Marvel-row');
         let contentHTML = '';
 
@@ -9,19 +12,18 @@ const marvel = {
             .then(res => res.json())
             .then((json) => {
                 for (const hero of json.data.results) {
-                    let urlHero = hero.url[0].url;
+                    let urlHero = hero.urls[0].url;
                     contentHTML += `
-                    <div class="col-md-4">
-                <a href="${urlHero}" target="_blank">
-                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" 
-                    class="img-thumbnail">
-                </a>
-                <h3 class= "title">${hero.name}</h3>
-            </div>`
+                      <div class="col-md-3">
+                          <a href="${urlHero}" target="_blank">
+                            <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                          </a>
+                          <h3 class="title">${hero.name}</h3>
+                      </div>`;
                 }
-                container.innerHTML = contentHTML
+                container.innerHTML = contentHTML;
             })
     }
-}
+};
 
 marvel.render();
